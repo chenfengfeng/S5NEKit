67852fc9d554739147efa65fbfa7bf109d32918e
Feng <zefeng@live.cn>
2020-01-15 16:13:58 +0800

    MMDB database

--- src/GeoIP/GeoIP.swift
+++ src/GeoIP/GeoIP.swift
@@ -2,7 +2,7 @@ import Foundation
 import MMDB
 
 open class GeoIP {
-    public static let database = MMDB()!
+    public static var database: MMDB!
 
     public static func LookUp(_ ipAddress: String) -> MMDBCountry? {
         return GeoIP.database.lookup(ipAddress)
